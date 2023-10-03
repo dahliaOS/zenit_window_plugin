b689d011d9c1b5da374a851628c1552b18130baf
Lars <larsb24@outlook.com>
2023-10-03 23:45:51 +0200

    initial code

---
+++ lib/zenit_window_plugin.dart
@@ -0,0 +1,5 @@
+library zenit_window_plugin;
+
+export "package:yaru_window/yaru_window.dart";
+export 'package:zenit_window_plugin/widgets/window_backdrop.dart';
+export "package:zenit_window_plugin/widgets/window_titlebar.dart";
